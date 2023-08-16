d6415145dfb40e42eac516a09051592cf0b94bdd
enjoys <deadly.info@gmail.com>
2023-08-16 14:31:22 +0300

    fix namespace

--- src/CKEditor5.php
+++ src/CKEditor5.php
@@ -4,9 +4,10 @@
 namespace EnjoysCMS\ContentEditor\CKEditor5;
 
 use Enjoys\AssetsCollector;
-use EnjoysCMS\ContentEditor\Summernote\NotSetupVendor;
-use EnjoysCMS\Core\Components\ContentEditor\ContentEditorInterface;
-use Psr\Log\LoggerInterface;
+use EnjoysCMS\Core\ContentEditor\ContentEditorInterface;
+use Exception;
+use RuntimeException;
+use Throwable;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -17,23 +18,22 @@ class CKEditor5 implements ContentEditorInterface
     private ?string $selector = null;
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function __construct(
-        private Environment $twig,
-        private AssetsCollector\Assets $assets,
-        private LoggerInterface $logger,
-        private ?string $template = null
+        private readonly Environment $twig,
+        private readonly AssetsCollector\Assets $assets,
+        private readonly ?string $template = null
     ) {
         if (!file_exists(__DIR__ . '/../node_modules/@ckeditor')) {
-            $command = sprintf('cd %s && yarn install', realpath(__DIR__.'/..'));
+            $command = sprintf('cd %s && yarn install', realpath(__DIR__ . '/..'));
             try {
                 $result = passthru($command);
-                if ($result === false){
-                    throw new \Exception();
+                if ($result === false) {
+                    throw new Exception();
                 }
-            }catch (\Throwable){
-                throw new \RuntimeException(sprintf('Run: %s', $command));
+            } catch (Throwable) {
+                throw new RuntimeException(sprintf('Run: %s', $command));
             }
         }
 
@@ -43,12 +43,12 @@ class CKEditor5 implements ContentEditorInterface
 
     public function initialize(): void
     {
-        $this->assets->add('js',__DIR__ . '/../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js');
+        $this->assets->add('js', __DIR__ . '/../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js');
     }
 
     private function getTemplate(): ?string
     {
-        return $this->template ?? __DIR__.'/../template/basic.twig';
+        return $this->template ?? __DIR__ . '/../template/basic.twig';
     }
 
 
@@ -60,7 +60,7 @@ class CKEditor5 implements ContentEditorInterface
     public function getSelector(): string
     {
         if ($this->selector === null) {
-            throw new \RuntimeException('Selector not set');
+            throw new RuntimeException('Selector not set');
         }
         return $this->selector;
     }
@@ -74,7 +74,7 @@ class CKEditor5 implements ContentEditorInterface
     {
         $twigTemplate = $this->getTemplate();
         if (!$this->twig->getLoader()->exists($twigTemplate)) {
-            throw new \RuntimeException(
+            throw new RuntimeException(
                 sprintf("ContentEditor: (%s): Нет шаблона в по указанному пути: %s", self::class, $twigTemplate)
             );
         }
